75c92e74c0829b98e212f57bc4c50362985bc4c3
pornimasaste <37161506+pornimasaste@users.noreply.github.com>
2026-02-11 10:04:08 +0530

    Add files via upload

--- Base/BaseTest.cs
+++ Base/BaseTest.cs
@@ -1,1 +1,58 @@
+﻿using AventStack.ExtentReports;
+using OpenCart.Reporting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace OpenCart.Base
+{
+    public class BaseTest
+        {
+        protected IWebDriver driver;
+        protected ExtentReports extent;
+        protected ExtentTest test;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            extent = ExtentReport.GetReport();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                test.Fail("Test Failed");
+                test.Fail(TestContext.CurrentContext.Result.Message);
+            }
+            else
+            {
+                test.Pass("Test Passed");
+            }
+
+            driver.Quit();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            extent.Flush();
+        }
+    }
+}
+    }
+}
