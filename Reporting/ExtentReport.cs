11225539688c1e10792bf3da985876b927ad4579
pornimasaste <37161506+pornimasaste@users.noreply.github.com>
2026-02-11 10:06:08 +0530

    Add files via upload

--- Reporting/ExtentReport.cs
+++ Reporting/ExtentReport.cs
@@ -1,1 +1,35 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace OpenCart.Reporting
+{
+    public class ExtentReport
+    {
+        private static ExtentReports extent;
+        private static ExtentSparkReporter spark;
+
+        public static ExtentReports GetReport()
+        {
+            if (extent == null)
+            {
+                string reportPath =
+                    AppDomain.CurrentDomain.BaseDirectory +
+                    @"\Reports\ExtentReport.html";
+
+                spark = new ExtentSparkReporter(reportPath);
+                spark.Config.DocumentTitle = "Automation Test Report";
+                spark.Config.ReportName = "Selenium Test Execution";
+                spark.Config.Theme = AventStack.ExtentReports.Reporter.Config.Theme.Standard;
+
+                extent = new ExtentReports();
+                extent.AttachReporter(spark);
+
+               
+            }
+            return extent;
+        }
+    }
+}
