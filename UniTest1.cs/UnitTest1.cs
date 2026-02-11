6758461fc200dcf91ef52edd4d757d9730024472
pornimasaste <37161506+pornimasaste@users.noreply.github.com>
2026-02-11 10:09:22 +0530

    Add files via upload

---
+++ UniTest1.cs/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using AventStack.ExtentReports;
+using OpenCart.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OpenCart
+{
+    public class CheckOutFlow
+    {
+       
+
+        [Test]
+        public void TestScenario1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demo.opencart.com/en-gb/catalog/tablet");
+            Thread.Sleep(15000);
+            AddtoCart cart = new AddtoCart(driver);
+            cart.itemClick();
+            checkoutPage checkoutpage = new checkoutPage(driver);
+            checkoutpage.fillEstimationDetails();
+            protected ExtentReports extent;
+
+    }
+
+    }
+}
